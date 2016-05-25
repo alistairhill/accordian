3b18a7652414be2c796f80c994f278f970bed7b8
Alistair <plantonly@gmail.com>
2016-05-24 23:12:55 -0700

    little tidy up

--- scripts/script.js
+++ scripts/script.js
@@ -6,15 +6,16 @@ function goGo() {
       if (e.target.tagName.toLowerCase() === "i") {
         var li = e.target.parentNode.parentNode,
         subMenu = li.lastElementChild;
-      if (subMenu.tagName.toLowerCase() === "ul") {
-        if (subMenu.style.maxHeight === "500px") {
-          subMenu.style.maxHeight = "0px";
-          e.target.innerHTML = "+";
-        } else {
-          e.target.innerHTML = "-";
-          subMenu.style.maxHeight = "500px";
+
+        if (subMenu.tagName.toLowerCase() === "ul") {
+          if (subMenu.style.maxHeight === "500px") {
+            subMenu.style.maxHeight = "0px";
+            e.target.innerHTML = "+";
+          } else {
+            e.target.innerHTML = "-";
+            subMenu.style.maxHeight = "500px";
+          }
         }
-      }
       }
     })
   }
