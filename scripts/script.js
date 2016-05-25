26a68d1d45bb88efbf359f0e7d67a1c03da91efc
Alistair <plantonly@gmail.com>
2016-05-24 22:12:44 -0700

    added expand functionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,23 @@
 function goGo() {
-  console.log("hi");
+  function menuOpen() {
+    var menu = document.querySelector("nav.main-menu");
+
+    menu.addEventListener("click", function(e) {
+      if (e.target.tagName.toLowerCase() === "i") {
+        var li = e.target.parentNode.parentNode,
+        subMenu = li.lastElementChild;
+      if (subMenu.tagName.toLowerCase() === "ul") {
+        if (subMenu.style.maxHeight === "500px") {
+          subMenu.style.maxHeight = "0px";
+        } else {
+          subMenu.style.maxHeight = "500px";
+        }
+      }
+      }
+    })
+  }
+
+  menuOpen();
 }
 
 window.onload = goGo();
